02aed5a18120077231a88f36d4344f79748d7cba
MahmoudElmahalawy <elmehalawy7@gmail.com>
2021-01-03 16:06:39 +0200

    Organised CSS files and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,2 @@
-var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested');
-
-
-gulp.task('default', function(){
-    console.log("You created a gulp task!");
-});
-
-gulp.task('styles', function(){
-    gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function(){
-    watch('./app/index.html', function(){
-        gulp.start('html');
-    });
-    watch('./app/assests/styles/**/*.css', function(){
-        gulp.start('styles');
-    });
-});
-
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
