5c8e1dcb74cc4457cc9d0cf332ca09ef15cbec3b
MahmoudElmahalawy <elmehalawy7@gmail.com>
2021-01-19 18:37:48 +0200

    Finished site header

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,24 @@
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+
+class MobileMenu{
+    constructor(){
+        this.siteHeader = $(".site-header");
+        this.menuIcon = $(".site-header__menu-btn");
+        this.menuContent = $(".site-header__menu-content");
+        this.events();
+    }
+
+    events(){
+        this.menuIcon.click(this.toggleTheMenu.bind(this));
+
+    }
+
+    toggleTheMenu(){
+        this.menuContent.toggleClass("site-header__menu-content--is-visible"); 
+        this.siteHeader.toggleClass("site-header--is-expanded");
+        this.menuIcon.toggleClass("site-header__menu-btn--close-x");
+    }
+}
+
+export default MobileMenu;
